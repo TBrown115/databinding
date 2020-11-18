37a4c28f5338270a5c5c1fc5eb5eebe6ddc3fc05
TBrown115 <70948046+TBrown115@users.noreply.github.com>
2020-11-18 15:20:44 +0200

    first commit

---
+++ data-binding-exercise/mslearn-xamarin-forms-data-binding/src/exercise1/final/FlagData/Flag.cs
@@ -0,0 +1,58 @@
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+
+
+namespace FlagData
+{
+    /// <summary>
+    /// This model object represents a single flag
+    /// </summary>
+    public class Flag : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Name of the country that this flag belongs to
+        /// </summary>
+        private void RaisePropertyChanged([CallerMemberName] string propertyName = "")
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+    public string Country { get; set; }
+        /// <summary>
+        /// Image URL for the flag (from resources)
+        /// </summary>
+        public string ImageUrl { get; set; }
+        /// <summary>
+        /// The date this flag was adopted
+        /// </summary>
+        private DateTime _dateAdopted;
+        public DateTime DateAdopted
+        {
+            get { return _dateAdopted; }
+            set
+            {
+                if (_dateAdopted != value)
+                {
+                    _dateAdopted = value;
+                    // Can pass the property name as a string,
+                    // -or- let the compiler do it because of the
+                    // CallerMemberNameAttribute on the RaisePropertyChanged method.
+                    RaisePropertyChanged();
+                }
+            }
+        }
+        /// <summary>
+        /// Whether the flag includes an image/shield as part of the design
+        /// </summary>
+        public bool IncludesShield { get; set; }
+        /// <summary>
+        /// Some trivia or commentary about the flag
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// A URL for more information
+        /// </summary>
+        public Uri MoreInformationUrl { get; set; }
+    }
+}
